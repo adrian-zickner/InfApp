31cd22b6ca6a2abc55a38569be39ca4ff2f754c2
Adrian Zickner <a.zickner@zickner.com>
2024-06-14 08:36:20 +0200

    GUI erstellt

---
+++ app/src/main/java/infapp/GUI.java
@@ -0,0 +1,15 @@
+package InfApp.app.src.main.java.infapp;
+
+import javax.swing.JFrame;
+
+public class GUI {
+    public static void main(String[] args) {
+
+    }
+
+    public void showStuff() {
+        JFrame frame = new JFrame();
+        frame.setSize(600, 600);
+        frame.setVisible(true);
+    }
+}
