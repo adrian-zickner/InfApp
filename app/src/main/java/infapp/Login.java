b1190839b5324777c6ebfa5126a31e9a6e94c7e7
Adrian Zickner <a.zickner@zickner.com>
2024-06-23 17:36:00 +0200

    fuer diagramm

--- app/src/main/java/infapp/Login.java
+++ app/src/main/java/infapp/Login.java
@@ -1,4 +1,5 @@
 package infapp;
+
 import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.event.*;
@@ -10,126 +11,100 @@ public class Login {
     private DBVerbindung verbindung;
     GUI gui = new GUI();
     int userkey = 0;
-    
 
     public void showStuff() {
-
 
         try {
             this.verbindung = new DBVerbindung("localhost", "projekt", "root", "");
             this.verbindung.open();
-            
+
         } catch (ClassNotFoundException | SQLException ex) {
             System.out.println("Verbindung fehlerhaft!");
             // TODO: handle exception
         }
 
-
         Border blackline;
         blackline = BorderFactory.createLineBorder(Color.black);
-
-
 
         JFrame frame = new JFrame();
         frame.setSize(600, 600);
         frame.setVisible(true);
 
-
-
         JTextField usernameTextfield = new JTextField("User eingeben");
         usernameTextfield.setBounds(250, 250, 100, 20);
-        
 
         JPasswordField passwordTextField = new JPasswordField("Passw eingeben");
         passwordTextField.setBounds(250, 300, 100, 20);
-                
 
         usernameTextfield.setBorder(blackline);
 
         JButton loginButton = new JButton("Login");
-        loginButton.setBounds(250,350,100,30);
+        loginButton.setBounds(250, 350, 100, 30);
 
         JButton registerUser = new JButton("Neu Registrieren");
-        registerUser.setBounds(225,400,150,30);
+        registerUser.setBounds(225, 400, 150, 30);
 
-        loginButton.addActionListener((ActionListener) new ActionListener(){  
-            public void actionPerformed(ActionEvent e){  
-                        try {
-                           verbindung.open();
-                        } catch (ClassNotFoundException | SQLException e1) {
-                           // TODO Auto-generated catch block
-                           e1.printStackTrace();
-                        }
+        loginButton.addActionListener((ActionListener) new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    verbindung.open();
+                } catch (ClassNotFoundException | SQLException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
 
+                String UserValue = usernameTextfield.getText();
+                char[] password = passwordTextField.getPassword();
 
-                        String UserValue = usernameTextfield.getText();
-                        char[] password = passwordTextField.getPassword();
+                String PasswValue = new String(password);
 
-                        String PasswValue = new String(password);
+                if (verbindung.checkUser(UserValue, PasswValue)) {
+                    frame.dispose();
+                    gui.setVisible(true);
 
-                        if (verbindung.checkUser(UserValue, PasswValue)) {
-                            frame.dispose();
-                            gui.setVisible(true);
-                            
-                        }else{
-                            JOptionPane.showMessageDialog(frame, "Password oder Username falsch!");
-                        }
-                    }  
-                });  
+                } else {
+                    JOptionPane.showMessageDialog(frame, "Password oder Username falsch!");
+                }
+            }
+        });
 
-        registerUser.addActionListener((ActionListener) new ActionListener(){  
-            public void actionPerformed(ActionEvent e){  
-                        try {
-                            verbindung.open();
-                        } catch (ClassNotFoundException | SQLException e1) {
-                            // TODO Auto-generated catch block
-                            e1.printStackTrace();
-                        }
+        registerUser.addActionListener((ActionListener) new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    verbindung.open();
+                } catch (ClassNotFoundException | SQLException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
 
-                        String UserValue = usernameTextfield.getText();
-                        char[] password = passwordTextField.getPassword();
+                String UserValue = usernameTextfield.getText();
+                char[] password = passwordTextField.getPassword();
 
-                        String PasswValue = new String(password);
-                        userkey++;
-                        registerUser(UserValue, PasswValue);
+                String PasswValue = new String(password);
+                userkey++;
+                registerUser(UserValue, PasswValue);
 
-
-
-
-                    }  
-                });          
+            }
+        });
 
         frame.add(loginButton);
         frame.add(usernameTextfield);
         frame.add(passwordTextField);
 
-
         frame.add(registerUser);
-
 
         frame.setLayout(null);
         frame.setVisible(true);
 
-
-
     }
 
-
-
-
-
-
-    public void registerUser( String username, String password) {
-
+    public void registerUser(String username, String password) {
 
         JFrame error = new JFrame(); // Declare the frame variable
         if (verbindung.checkUser(username, password)) {
             JOptionPane.showMessageDialog(error, "Username bereits registriert!");
             return;
         }
-        
-         
-
 
         String sql = "INSERT INTO user(username, password) VALUES (?, ?)";
         try {
@@ -145,9 +120,7 @@ public class Login {
                 e.printStackTrace();
             }
         }
-            
+
     }
-
-
 
 }
--- app/src/main/java/infapp/Notiz.java
+++ app/src/main/java/infapp/Notiz.java
@@ -1,28 +1,42 @@
 package infapp;
 
+import java.sql.SQLException;
+
 public class Notiz {
+
+    DBVerbindung verbundung;
+
     private int N_ID;
     private String Inhalt;
     private String Kategorie;
     private int U_ID;
 
-    public void holeNotiz(int NotizID){
-        /*TODO: METHODE GREIFT DIE NOTIZEN 
-         AUS DB ANHAND N_ID
-         -- Marcello Pirillo --
-       */
-    }
-    public void löscheNotiz(int NotizID){
-        //Löschen aus DB
-        
-    }
-
-    public void bearbeiteNotiz(){
-     /*
-        TODO: Anhand von holeNotiz diese dann bearbeiten 
-       -- Marcello Pirillo --
-     */
+    public void holeNotiz(int NotizID) {
+        String sql = "INSERT INTO notiz(Titel, Inhalt, Kategorie) VALUES (?, ?, ?)";
+        try {
+            verbindung.prepareAndExecuteStatement(sql, titel, "null", "null");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Fehler bei Ezeugung!");
+        } finally {
+            try {
+                verbindung.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    
+    public void löscheNotiz(int NotizID) {
+        // Löschen aus DB
+
+    }
+
+    public void bearbeiteNotiz() {
+        /*
+         * TODO: Anhand von holeNotiz diese dann bearbeiten
+         * -- Marcello Pirillo --
+         */
+    }
+
 }
